b64f93306808802734a51ab5c1765cd015dc40ea
Nicolas Bouissou <bouissou.n@outlook.fr>
2023-12-29 16:14:36 +0100

    fix: static host

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -3,7 +3,7 @@ const configure = require('./configure')
 
 const config = configure()
 var db = redis.createClient({
-  host: config.redis.host,
+  host: 127.0.0.1,
   port: 6379,
   retry_strategy: () => {
     return new Error("Retry time exhausted")
