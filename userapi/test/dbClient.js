fea07301291cd539d015015b4ad92ca3be162a07
Nicolas Bouissou <bouissou.n@outlook.fr>
2023-12-29 15:49:22 +0100

    fix: tests

--- userapi/test/dbClient.js
+++ userapi/test/dbClient.js
@@ -1,18 +1,14 @@
-const { expect } = require('chai');
+const { expect } = require('chai')
 
-let db;
+let db
 
 describe('Redis', () => {
+  
   before(() => {
-    db = require('../src/dbClient');
-  });
-
-  it('should connect to Redis', (done) => {
-    
-    db.on('ready', () => {
-      
-      expect(db.connected).to.eql(true);
-      done(); 
-    });
-  });
-});
+    db = require('../src/dbClient')
+  })
+  
+  it('should connect to Redis', () => {
+    expect(db.connected).to.eql(true)
+  })
+})
