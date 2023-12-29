d75d9b017cc193588afea4b0a27f851d97cca442
Nicolas Bouissou <bouissou.n@outlook.fr>
2023-12-29 16:11:49 +0100

    fix: specifying the port

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -4,26 +4,14 @@ const configure = require('./configure')
 const config = configure()
 var db = redis.createClient({
   host: config.redis.host,
-  port: config.redis.port,
+  port: 6379,
   retry_strategy: () => {
     return new Error("Retry time exhausted")
   }
 })
 
-// Log when the Redis client connects
-db.on('connect', () => {
-  console.log('Connected to Redis');
-});
-
-// Log when an error occurs
-db.on('error', (err) => {
-  console.error('Redis error:', err);
-});
-
 process.on('SIGINT', function() {
-  // Log before quitting
-  console.log('Closing Redis connection due to SIGINT');
   db.quit();
 });
 
-module.exports = db;
+module.exports = db
