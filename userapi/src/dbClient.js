c3dd9c68cc257024a6e577bb7f377597882a6757
Nicolas Bouissou <bouissou.n@outlook.fr>
2023-12-29 16:22:56 +0100

    fix: db client fix

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -3,8 +3,8 @@ const configure = require('./configure')
 
 const config = configure()
 var db = redis.createClient({
-  host: '127.0.0.1',
-  port: 6379,
+  host: config.redis.host,
+  port: config.redis.port,
   retry_strategy: () => {
     return new Error("Retry time exhausted")
   }
